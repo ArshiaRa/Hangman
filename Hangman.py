82226f042f9e2f7997f2b53884bef0a5e8f75843
Arshia <arshiarashidi98@gmail.com>
2020-05-20 17:13:35 +0430

    Update
    Dash

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,18 @@
 import random
+            
 words = ['python', 'java', 'kotlin', 'javascript']
 print("H A N G M A N")
 s = random.choice(words)
-print("Guess the word:", s[:3]+ '-' * (len(s) - 3 ))
-if s == input():
-    print("You survived!")
-else:
+print(s)
+guess = []
+print(type(guess))
+guess = '-' * (len(s))
+while True:
+    print("Input a letter:")
+    letter = input()
+    if letter in s:
+        for i in range(len(s)):
+            if s[i] == letter:
+                guess = guess[:i] + letter + guess[i+1:]
+                print(guess)
     print("You are hanged!")
